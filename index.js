6f26c19acea00e694549bd74be7e283ff082a695
Ujjwal Pandey <ujjwalpandey922@gmail.com>
2022-09-22 14:56:55 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,340 @@
+let req = {
+  status: "ok",
+  totalResults: 38,
+  articles: [
+    {
+      source: {
+        id: null,
+        name: "NDTV News",
+      },
+      author: null,
+      title:
+        "\"Whether It's Ashok Gehlot Or Me...\": Sachin Pilot To NDTV On Chief Minister's Job - NDTV",
+      description:
+        "Sachin Pilot was asked if he'll get Rajasthan post owing to idea that Ashok Gehlot shouldn't hold two posts together upon becoming Congress chief",
+      url: "https://www.ndtv.com/india-news/on-ashok-gehlot-contesting-for-congress-president-sachin-pilot-says-this-3363451",
+      urlToImage:
+        "https://c.ndtvimg.com/2019-06/up3oadp8_ashok-gehlot-sachin-pilot-pti_625x300_04_June_19.jpg",
+      publishedAt: "2022-09-21T10:11:00Z",
+      content:
+        "Kochi (Kerala): Congress leader Sachin Pilot today walked around the questions on his chances of becoming Chief Minister of Rajasthan if Ashok Gehlot becomes Congress President. \"It's not up to me. T… [+3294 chars]",
+    },
+    {
+      source: {
+        id: null,
+        name: "Beebom",
+      },
+      author: "Vanshika Malhotra",
+      title:
+        "Asus Vivobook 14 Touch with 12th Gen Processor Launched in India - Beebom",
+      description:
+        "Asus has launched the new Vivobook 14 Touch with a 12th Gen Intel processor, touch screen, and more. See price, features, specs.",
+      url: "https://beebom.com/asus-vivobook-14-touch-launched-india/",
+      urlToImage:
+        "https://beebom.com/wp-content/uploads/2022/09/asus-vivobook-14-touch.jpg",
+      publishedAt: "2022-09-21T10:06:12Z",
+      content:
+        "Asus has introduced a new Vivobook laptop called the Vivobook 14 Touch in India. The laptop comes with the latest 12th Gen Intel processor on board, a touch screen, and a portable design, among other… [+1266 chars]",
+    },
+    {
+      source: {
+        id: null,
+        name: "The Indian Express",
+      },
+      author: "Science Desk",
+      title:
+        "NASA's Artemis 1 cryogenic test demonstration: Watch live stream - The Indian Express",
+      description:
+        "NASA is on track to conduct an Artemis 1 cryogenic test demonstration starting on September 21. You can watch the demonstration live below.",
+      url: "https://indianexpress.com/article/technology/science/nasa-artemis-1-cryogenic-test-demonstration-8164256/",
+      urlToImage:
+        "https://images.indianexpress.com/2022/09/NASA-Artemis-1-cryogenic-test-20220919.jpg",
+      publishedAt: "2022-09-21T10:05:01Z",
+      content:
+        "NASA is on track to conduct an Artemis 1 cryogenic test demonstration starting on September 21. Since the last failed attempt to launch the Space Launch System (SLS) rocket, the space agencys teams h… [+1866 chars]",
+    },
+    {
+      source: {
+        id: null,
+        name: "Hindustan Times",
+      },
+      author: "Himani Jha",
+      title:
+        "The pricey iPhone 14 Pro mania grips the world as demand skyrockets - HT Tech",
+      description:
+        "A new report has revealed that the Apple iPhone 14 Pro demand is skyrocketing despite its high price.",
+      url: "https://tech.hindustantimes.com/tech/news/the-pricey-iphone-14-pro-mania-grips-the-world-as-demand-skyrockets-71663752878112.html",
+      urlToImage:
+        "https://images.hindustantimes.com/tech/img/2022/09/21/1600x900/IMG_4716_1663335524279_1663753662410_1663753662410.jpg",
+      publishedAt: "2022-09-21T09:49:15Z",
+      content:
+        "On September 7, the Cupertino-based tech giant unveiled the iPhone 14 series of smartphones. The series includes four models two with modest features and two with top-end specs, but only three models… [+1705 chars]",
+    },
+    {
+      source: {
+        id: "the-times-of-india",
+        name: "The Times of India",
+      },
+      author: "ET Online",
+      title:
+        "Vladimir Putin announces partial mobilisation for Russian citizens - Economic Times",
+      description:
+        "Putin's address to the nation comes a day after Russian-controlled regions in eastern and southern Ukraine announced plans to hold votes on becoming integral parts of Russia. The Kremlin-backed efforts to swallow up four regions could set the stage for Moscow…",
+      url: "https://economictimes.indiatimes.com/news/international/world-news/vladimir-putin-announces-partial-mobilisation-for-russian-citizens/articleshow/94344245.cms",
+      urlToImage:
+        "https://img.etimg.com/thumb/msid-94344475,width-1070,height-580,imgsize-44990,overlay-economictimes/photo.jpg",
+      publishedAt: "2022-09-21T09:48:00Z",
+      content:
+        "Russian President Vladimir Putin on Wednesday announced a partial mobilisation in Russia as the war in Ukraine reaches nearly seven months.\r\nPutin's address to the nation comes a day after Russian-co… [+3269 chars]",
+    },
+    {
+      source: {
+        id: null,
+        name: "NDTV News",
+      },
+      author: null,
+      title:
+        "Comedian Raju Srivastava, 58, Dies Weeks After Cardiac Arrest In Gym - NDTV",
+      description:
+        "Comedian Raju Srivastava died today in Delhi, more than a month after he was hospitalized after suffering a cardiac arrest. He was 58.",
+      url: "https://www.ndtv.com/india-news/comedian-raju-srivastava-dies-at-58-he-was-in-aiims-delhi-since-cardiac-arrest-last-month-3363310",
+      urlToImage:
+        "https://c.ndtvimg.com/2022-09/lc2vaj7_comedian-raju-srivastava-650_650x400_21_September_22.jpg",
+      publishedAt: "2022-09-21T09:43:00Z",
+      content:
+        "Comedian Raju Srivastava will be cremated in Delhi on Thursday. (File)\r\nNew Delhi: Comedian Raju Srivastava died today in Delhi, over a month after he suffered a cardiac arrest while working out in t… [+1988 chars]",
+    },
+    {
+      source: {
+        id: null,
+        name: "Livelaw.in",
+      },
+      author: "LIVELAW NEWS NETWORK",
+      title:
+        "Hijab Ban- Supreme Court Hearing- DAY-9 Live Updates - Live Law - Indian Legal News",
+      description:
+        "Supreme Court bench comprising Justices Hemant Gupta and Sudhanshu Dhulia will hear a batch of...",
+      url: "https://www.livelaw.in/top-stories/karnataka-hijab-ban-supreme-court-erp-muslim-students-209852",
+      urlToImage:
+        "https://www.livelaw.in/h-upload/2022/09/12/434570-hijab-ban.jpeg",
+      publishedAt: "2022-09-21T09:31:49Z",
+      content:
+        "Supreme Court bench comprising Justices Hemant Gupta and Sudhanshu Dhulia will hear a batch of petitions challenging the ban on wearing Hijab in educational institutions in Karnataka.\r\nA batch of 23 … [+980 chars]",
+    },
+    {
+      source: {
+        id: null,
+        name: "NDTV News",
+      },
+      author: null,
+      title: "Ratan Tata Joins As Trustee Of PM CARES Fund - NDTV",
+      description:
+        "Prime Minister Narendra Modi has lauded the people for contributing wholeheartedly to the PM CARES Fund as he chaired a meeting where it was discussed that the initiative has a larger vision on effectively responding to emergency situations",
+      url: "https://www.ndtv.com/india-news/pm-narendra-modi-ratan-tata-pm-cares-fund-ratan-tata-joins-as-trustee-of-pm-cares-3364029",
+      urlToImage:
+        "https://c.ndtvimg.com/2022-04/pm2b47a8_ratan-tata-modi-pti-650_625x300_28_April_22.jpg",
+      publishedAt: "2022-09-21T09:30:16Z",
+      content:
+        "Ratan Tata attended the meeting chaired by the Prime Minister. File\r\nNew Delhi: Prime Minister Narendra Modi has lauded the people for contributing wholeheartedly to the PM CARES Fund as he chaired a… [+2102 chars]",
+    },
+    {
+      source: {
+        id: null,
+        name: "NDTV News",
+      },
+      author: null,
+      title:
+        "Viral: Richa Chadha And Ali Fazal's Quirky Wedding Invite - NDTV Movies",
+      description:
+        "Richa Chadha and Ali Fazal will reportedly get married on October4",
+      url: "https://www.ndtv.com/entertainment/viral-richa-chadha-and-ali-fazals-quirky-wedding-invite-3364030",
+      urlToImage:
+        "https://c.ndtvimg.com/2022-09/1ln043sg_-richa-chadha_625x300_21_September_22.jpg",
+      publishedAt: "2022-09-21T09:26:04Z",
+      content:
+        "Richa Chadha and Ali Fazal pictured together (L), a picture of the wedding invite. (R)\r\nNew Delhi: Star couple Richa Chadha and Ali Fazal are all set for an October 4 wedding, going by the many repor… [+1513 chars]",
+    },
+    {
+      source: {
+        id: null,
+        name: "The Indian Express",
+      },
+      author: "Tech Desk",
+      title:
+        "Amazon Great Indian Festival sale 2022: Best deals on Samsung Galaxy smartphones - The Indian Express",
+      description:
+        "Here are some of the best offers you can avail on Samsung Galaxy phones during the Amazon Great Indian Festival.",
+      url: "https://indianexpress.com/article/technology/mobile-tabs/amazon-great-indian-festival-sale-2022-best-deals-on-samsung-smartphones-8163907/",
+      urlToImage:
+        "https://images.indianexpress.com/2022/09/Samsung-Galaxy-S20-FE-5G.jpg",
+      publishedAt: "2022-09-21T09:22:56Z",
+      content:
+        "Ahead of the Amazon Great Indian Festival sale, Samsung has officially unveiled several offers on premium, mid-range and budget devices. The South-Korean smartphone maker is offering huge discounts o… [+2792 chars]",
+    },
+    {
+      source: {
+        id: null,
+        name: "India Today",
+      },
+      author: null,
+      title:
+        "Nasa's Dart mission takes heavenly route to intentionally crash on asteroid - India Today",
+      description:
+        "The DART mission is set to crash on an asteroid in a few days, but before it reaches its destination, it has used Jupiter as a practice target.",
+      url: "https://www.indiatoday.in/science/story/nasa-s-dart-mission-takes-heavenly-route-to-intentionally-crash-on-asteroid-2002852-2022-09-21",
+      urlToImage:
+        "https://akm-img-a-in.tosshub.com/indiatoday/images/story/202209/Jupiters-647x363.jpeg?IAylrTpAUR5iwKNrV8jfffp2NdMFH9k4",
+      publishedAt: "2022-09-21T08:09:37Z",
+      content:
+        "The Double Asteroid Redirection Test (DART) mission is inching closer to its final days when it intentionally crashes on an asteroid to test a key technology that could be used to deflect asteroids h… [+2683 chars]",
+    },
+    {
+      source: {
+        id: null,
+        name: "Asianetnews.com",
+      },
+      author: "Team Newsable",
+      title:
+        "Heart Attack: Vomiting to nausea and more unexpected warning signs you should look out for - Asianet Newsable ",
+      description:
+        "Heart attacks have become a rising cause of death these days. Your body gives you particular signals- you risk a heart attack. Here are some symptoms of a silent heart attack.",
+      url: "https://newsable.asianetnews.com/lifestyle/heart-attack-vomiting-to-nausea-and-more-unexpected-warning-signs-you-should-look-out-for-sur-rijvn4",
+      urlToImage:
+        "https://static-ai.asianetnews.com/images/01gdfgy8sqg5vzjhqnf0c23gff/whatsapp-image-2022-09-21-at-12-45-21-pm_1200x630xt.jpg",
+      publishedAt: "2022-09-21T08:01:53Z",
+      content:
+        "Heart attacks have become a rising cause of death these days. Your body gives you particular signals- you risk a heart attack. Here are some symptoms of a silent heart attack. We can see many young p… [+1987 chars]",
+    },
+    {
+      source: {
+        id: null,
+        name: "News-Medical.Net",
+      },
+      author: null,
+      title:
+        "World Alzheimer’s Day 2022: Improving Post-Diagnosis Care with COGNISANCE - News-Medical.Net",
+      description:
+        "To commemorate World Alzheimer's Day, News-Medical speaks to Dr. Meredith Gresham, coordinator of COGNISANCE: Co-Designing Dementia Diagnosis And Post Diagnostic Care, about the project and post-dementia diagnosis care for patients and their families.",
+      url: "https://www.news-medical.net/news/20220921/World-Alzheimere28099s-Day-2022-Improving-Post-Diagnosis-Care-with-COGNISANCE.aspx",
+      urlToImage:
+        "https://d2jx2rerrg6sh3.cloudfront.net/images/news/ImageForNews_725009_16630782262296608.png",
+      publishedAt: "2022-09-21T07:45:00Z",
+      content:
+        "To commemorate World Alzheimer's Day, News-Medical speaks to Dr. Meredith Gresham, coordinator of COGNISANCE: Co-Designing Dementia Diagnosis And Post Diagnostic Care, about the project and post-deme… [+15301 chars]",
+    },
+    {
+      source: {
+        id: null,
+        name: "Livelaw.in",
+      },
+      author: "LIVELAW NEWS NETWORK",
+      title:
+        "EWS Quota Case Hearing Day 5 - Live Updates From Supreme Court - Live Law - Indian Legal News",
+      description:
+        "SupremeCourt Constitution Bench to continue hearings on cases challenging the constitutional...",
+      url: "https://www.livelaw.in/top-stories/ews-quota-case-hearing-day-5-live-updates-from-supreme-court-209854",
+      urlToImage:
+        "https://www.livelaw.in/h-upload/2022/09/21/435976-366521-live-updates-sc.jpg",
+      publishedAt: "2022-09-21T07:30:17Z",
+      content:
+        "State of MP: Those in 16(4) have the same position as forward class minus the EWS. If EWS is held as valid, it'll be for both. \r\nCJI: So far as open category is concerned, you're reducing that from 5… [+9 chars]",
+    },
+    {
+      source: {
+        id: null,
+        name: "India Today",
+      },
+      author: null,
+      title:
+        "James Webb Telescope damaged in space, science ops using key instrument paused - India Today",
+      description:
+        "The world's most powerful observatory has hit a technical glitch and science ops using its MIR instrument have been paused.",
+      url: "https://www.indiatoday.in/science/story/james-webb-telescope-damaged-in-space-science-ops-using-key-instrument-paused-2002829-2022-09-21",
+      urlToImage:
+        "https://akm-img-a-in.tosshub.com/indiatoday/images/story/202209/Jwst_22_2-647x363.jpeg?OInSTPctTFyIvnd1AGBv7NXcHGwp2ec0",
+      publishedAt: "2022-09-21T07:29:52Z",
+      content:
+        "Located 1,50,000 kilometers away from Earth, the James Webb Space Telescope is a marvel of engineering, but it remains vulnerable to the harsh space environment. The spacecraft has now run into a new… [+3157 chars]",
+    },
+    {
+      source: {
+        id: null,
+        name: "NDTV News",
+      },
+      author: null,
+      title:
+        '230 Pilot Whales Stranded In Australia, "About Half" Feared Dead - NDTV',
+      description:
+        "A pod of 230 pilot whales was found stranded on the rugged west coast of Tasmania Wednesday, with Australian officials saying only half appeared to be alive.",
+      url: "https://www.ndtv.com/world-news/230-pilot-whales-stranded-in-australia-about-half-feared-dead-3363615",
+      urlToImage:
+        "https://c.ndtvimg.com/2022-03/19ukk8v8_new-zealand-whales-stranding-afp_625x300_18_March_22.jpg",
+      publishedAt: "2022-09-21T07:23:54Z",
+      content:
+        "Locals doused the whales with buckets of water to keep them alive. (Representational)\r\nSydney: A pod of 230 pilot whales was found stranded on the rugged west coast of Tasmania Wednesday, with Austra… [+2808 chars]",
+    },
+
+    {
+      source: {
+        id: null,
+        name: "The Tribune India",
+      },
+      author: "The Tribune India",
+      title:
+        "After Chandigarh University video leak case, now students protest at Phagwara's Lovely Professional University over one of them 'dying by suicide' - The Tribune India",
+      description:
+        "A protest broke out on the Lovely Professional University campus near here on Tuesday night after a student allegedly died by suicide in the hostel.",
+      url: "https://www.tribuneindia.com/news/jalandhar/amid-cu-videos-leak-row-late-night-protest-in-lovely-professional-university-allegedly-over-student-committing-suicide-433845",
+      urlToImage:
+        "https://englishtribuneimages.blob.core.windows.net/gallary-content/2022/9/2022_9$largeimg_274135819.jpg",
+      publishedAt: "2022-09-21T06:24:00Z",
+      content:
+        "The Tribune, now published from Chandigarh, started publication on February 2, 1881, in Lahore (now in Pakistan). It was started by Sardar Dyal Singh Majithia, a public-spirited philanthropist, and i… [+505 chars]",
+    },
+    {
+      source: {
+        id: null,
+        name: "NDTV News",
+      },
+      author: null,
+      title:
+        "Livestreaming Of Supreme Court Constitution Bench Hearings From Next Week - NDTV",
+      description:
+        "People will be able to watch proceedings in major cases such as challenge to Citizenship Amendment Act and quota to Upper Castes on economic basis",
+      url: "https://www.ndtv.com/india-news/livestreaming-of-supreme-courts-constitution-bench-hearings-to-begin-next-week-3363180",
+      urlToImage:
+        "https://c.ndtvimg.com/2021-04/hmt8eqi8_supreme-court-_650x400_30_April_21.jpg",
+      publishedAt: "2022-09-21T06:22:00Z",
+      content:
+        "New Delhi: The Supreme Court will livestream all Constitutional Bench hearings from September 27 on its website, which means anyone can watch the proceedings in cases such as challenges to the Citize… [+3432 chars]",
+    },
+  ],
+};
+
+let singleCard = document.getElementById("cont");
+let source = req.articles;
+let newsHtml = "";
+source.forEach((element) => {
+  let singleNews = ` <div class="card">
+  <img src=${element.urlToImage} class="card-img-top" alt=${element.title} />
+  
+  <div class="card-body text-center">
+    <h5 class="card-title">${element.title.slice(0, 50)}...</h5>
+    <p class="card-text">${
+      element.description !== null
+        ? element.description.slice(0, 150)
+        : element.description
+    }...</p>
+    <a
+      href=${element.url}
+      rel="noreferrer"
+      target="_blank"
+      class="btn btn-danger "
+    >
+      Read More....
+    </a>          
+  </div>
+</div> `;
+  newsHtml += singleNews;
+});
+singleCard.innerHTML = newsHtml;
